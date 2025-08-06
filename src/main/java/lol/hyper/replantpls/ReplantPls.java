2a90150c038288f75202b002faffc7c18edabfad
hyperdefined <contact@hyper.lol>
2025-08-06 19:07:03 -0400

    hyperlib updates

--- src/main/java/lol/hyper/replantpls/ReplantPls.java
+++ src/main/java/lol/hyper/replantpls/ReplantPls.java
@@ -1,9 +1,8 @@
 package lol.hyper.replantpls;
 
 import lol.hyper.hyperlib.HyperLib;
-import lol.hyper.hyperlib.bstats.bStats;
-import lol.hyper.hyperlib.releases.modrinth.ModrinthPlugin;
-import lol.hyper.hyperlib.releases.modrinth.ModrinthRelease;
+import lol.hyper.hyperlib.bstats.HyperStats;
+import lol.hyper.hyperlib.releases.HyperUpdater;
 import lol.hyper.replantpls.mcmmo.MCMMOHook;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -20,8 +19,8 @@ public final class ReplantPls extends JavaPlugin {
         HyperLib hyperLib = new HyperLib(this);
         hyperLib.setup();
 
-        bStats bstats = new bStats(hyperLib, 24167);
-        bstats.setup();
+        HyperStats stats = new HyperStats(hyperLib, 24167);
+        stats.setup();
 
         if (Bukkit.getPluginManager().isPluginEnabled("mcMMO")) {
             logger.info("mcMMO is detected, enabling support for it!");
@@ -33,27 +32,11 @@ public final class ReplantPls extends JavaPlugin {
 
         Bukkit.getPluginManager().registerEvents(new PlayerInteract(this), this);
 
-        Bukkit.getAsyncScheduler().runNow(this, scheduledTask -> {
-            ModrinthPlugin modrinthPlugin = new ModrinthPlugin("qw91hiLm");
-            modrinthPlugin.get();
-
-            ModrinthRelease release = modrinthPlugin.getReleaseByVersion(this.getPluginMeta().getVersion());
-            if (release == null) {
-                logger.warning("You are running a version not published.");
-            } else {
-                int buildsBehind = modrinthPlugin.buildsVersionsBehind(release);
-                if (buildsBehind > 0) {
-                    ModrinthRelease latest = modrinthPlugin.getLatestRelease();
-                    if (latest != null) {
-                        logger.info("You are " + buildsBehind + " versions behind. Please update!");
-                        logger.info("The latest version is " + latest.getVersion());
-                        logger.info(latest.getVersionPage());
-                    }
-                } else {
-                    logger.info("Yay! You are running the latest version.");
-                }
-            }
-        });
+        HyperUpdater updater = new HyperUpdater(hyperLib);
+        updater.setGitHub("hyperdefined", "ReplantPls");
+        updater.setModrinth("qw91hiLm");
+        updater.setHangar("ReplantPls", "paper");
+        updater.check();
     }
 
     public MCMMOHook getMCMMOHook() {
